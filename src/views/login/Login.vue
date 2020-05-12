740281e6368f086af78c8677d53aacec5022d9bb
namecjk <312316773@qq.com>
2020-05-12 15:53:15 +0800

    创建login elemtn-ui

---
+++ src/views/login/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    this is login
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-button type="primary" disabled>主要按钮</el-button>
+    <el-button type="success" disabled>成功按钮</el-button>
+    <el-button type="info" disabled>信息按钮</el-button>
+    <el-button type="warning" disabled>警告按钮</el-button>
+    <el-button type="danger" disabled>危险按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style lang="sass" scope>
+    
+</style>
